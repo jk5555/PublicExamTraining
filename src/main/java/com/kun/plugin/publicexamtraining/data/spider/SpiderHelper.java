5148a71f7e7c4978e00f0f781e4f2589e2510fe0
Jiangkun <wb_jiangkun02@meituan.com>
2025-01-24 10:04:40 +0800

    开始写ui---11

--- src/main/java/com/kun/plugin/publicexamtraining/data/spider/SpiderHelper.java
+++ src/main/java/com/kun/plugin/publicexamtraining/data/spider/SpiderHelper.java
@@ -1,5 +1,6 @@
 package com.kun.plugin.publicexamtraining.data.spider;
 
+import org.apache.commons.lang3.StringUtils;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.scheduler.QueueScheduler;
 
@@ -7,6 +8,9 @@ public class SpiderHelper {
 
 
     public static void spiderPaper(String url) {
+        if (StringUtils.isBlank(url)) {
+            return;
+        }
         Spider.create(new OpenQuestionProcessor())
                 .addPipeline(new OpenQuestionPipeline())
                 .setScheduler(new QueueScheduler())//去重
